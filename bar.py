e7e7b0531864decb0aa6ae2ff4ac78fe1ce2fd0f
Willy Douhard <willy.douhard@gmail.com>
2024-06-06 23:49:01 +0200

    add bar

---
+++ bar.py
@@ -0,0 +1,10 @@
+from config import literal_client, openai_client
+
+
+@literal_client.step(type="run")
+def bar():
+    literal_client.message(content="Hello from foo!")
+    response = openai_client.chat.completions.create(model="gpt-3.5-turbo", messages=[{"role": "system", "content": "You are a helpful assistant."}])
+    literal_client.message(content=response.choices[0].message.content)
+    response = openai_client.chat.completions.create(model="gpt-3.5-turbo", messages=[{"role": "system", "content": "You are a helpful assistant."}])
+    literal_client.message(content=response.choices[0].message.content)
--- foo.py
+++ foo.py
@@ -1,10 +1,6 @@
-from config import literal_client, openai_client
+from config import openai_client
+from bar import bar
 
-
-@literal_client.step(type="run")
 def foo():
-    literal_client.message(content="Hello from foo!")
     response = openai_client.chat.completions.create(model="gpt-3.5-turbo", messages=[{"role": "system", "content": "You are a helpful assistant."}])
-    literal_client.message(content=response.choices[0].message.content)
-    response = openai_client.chat.completions.create(model="gpt-3.5-turbo", messages=[{"role": "system", "content": "You are a helpful assistant."}])
-    literal_client.message(content=response.choices[0].message.content)
+    bar()
